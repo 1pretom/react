d7587c61489fb0b9aff56c3734b0888ebf01c65e
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-18 12:34:29 -0300

    add sidebar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,27 @@
 import { Post } from "./Post";
-import { Header } from "./components/Header";
-import './global.css'
+
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export const App = () => {
   return (
-    <h1>
-      <Header/>
-      hola mondo
-      <Post author="wash" content="sim" />
-    </h1>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Wash Damacena"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post author="Marina Soares" content="Um novo post muito legal" />
+        </main>
+      </div>
+    </div>
   );
 };
--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import styles from "./Header.module.css";
-import babaLogo from '../assets/baba-logo.png'
-export const Header = () => {
+import babaLogo from '../../assets/baba-logo.png'
+
+export function Header () {
   return (
     <header className={styles.header}>
         <img src={babaLogo} alt="logo baba europeu" />
