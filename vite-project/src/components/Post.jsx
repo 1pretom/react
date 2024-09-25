18313261c205e31774c73a4d1bb0eed9e5c18a29
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-09-24 21:07:13 -0300

    add style to comment

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -38,8 +38,6 @@ export const Post = () => {
       </form>
       <div className={styles.commentList}>
         <Comment />
-        <Comment />
-        <Comment />
       </div>
     </article>
   );
