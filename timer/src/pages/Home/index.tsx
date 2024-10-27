816584b2e0adb340b196b9c681ee99d283be5e6e
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-10-27 20:19:38 -0300

    add zod

--- timer/src/pages/Home/index.tsx
+++ timer/src/pages/Home/index.tsx
@@ -1,9 +1,21 @@
 import { Play } from "phosphor-react";
 import * as S from "./styles";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
+});
 
 export const Home = () => {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
   function handleCreateNewCycle(data: any) {
     console.log(data);
   }
@@ -20,7 +32,7 @@ export const Home = () => {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
-            {...register('task')}
+            {...register("task")}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -37,7 +49,7 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
-            {...register('minutesAmount', { valueAsNumber: true })}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
