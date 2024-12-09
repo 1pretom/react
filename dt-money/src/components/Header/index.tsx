aa27dd7f60cee0ab11a0800d9f4e68b5bbbeff84
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-12-09 20:58:47 -0300

    add new transactions modal

--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,25 +1,23 @@
-import logoImg from "../../assets/logo.svg";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog';
 
+import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+
 export function Header() {
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={logoImg} alt="" />
-                <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <NewTransactionButton>Nova transação</NewTransactionButton>
-        </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay />
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-            </HeaderContent>
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
