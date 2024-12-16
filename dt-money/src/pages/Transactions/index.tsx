3c08403f508d2d618e425401ba87e8c4fda27d3c
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-12-15 21:58:45 -0300

    add transactions context

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
-import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { SearchForm } from "../../components/SearchForm";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, []);
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
@@ -33,6 +16,7 @@ export function Transactions() {
 
       <TransactionsContainer>
         <SearchForm />
+
         <TransactionsTable>
           <tbody>
             {transactions.map(transaction => {
@@ -46,7 +30,6 @@ export function Transactions() {
                   </td>
                   <td>{transaction.category}</td>
                   <td>{transaction.createdAt}</td>
-
                 </tr>
               )
             })}
