3f9327b4bfd74082da5ba7236921b497f35a9f2c
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-12-27 23:07:25 -0300

    update libs and improve sign in page

--- pizzashop-web/src/pages/layouts/auth.tsx
+++ pizzashop-web/src/pages/layouts/auth.tsx
@@ -1,37 +1,23 @@
-import { Helmet } from 'react-helmet-async'
+import { Pizza } from 'lucide-react'
+import { Outlet } from 'react-router-dom'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-
-export function SignIn() {
+export function AuthLayout() {
   return (
-    <>
-      <Helmet title="Login" />
-
-      <div className="p-8">
-        <div className="flex w-[350px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas pelo painel do parceiro!
-            </p>
-          </div>
-
-          <form className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
-            </div>
-
-            <Button className="w-full" type="submit">
-              Acessar painel
-            </Button>
-          </form>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
         </div>
+
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
       </div>
-    </>
+
+      <div className="flex flex-col items-center justify-center">
+        <Outlet />
+      </div>
+    </div>
   )
 }
