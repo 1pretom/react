1badaae37befff920ced35133f0a541ac8237ea6
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2025-01-23 23:59:22 -0300

    add mocks

--- pizzashop-web/src/api/mocks/index.ts
+++ pizzashop-web/src/api/mocks/index.ts
@@ -3,7 +3,23 @@ import { setupWorker } from 'msw/browser'
 import { env } from '@/env'
 
 import { signInMock } from './sign-in-mock'
-export const worker = setupWorker(signInMock)
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getMonthRevenueMock } from './get-month-revenue'
+import { getPopularProductsMock } from './get-popular-products-mock'
+
+export const worker = setupWorker(signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
