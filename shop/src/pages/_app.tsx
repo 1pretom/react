fa8a6d72d9384b836f62ba2f9359ea18fa66ac73
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2025-02-06 23:15:59 -0300

    cabecalho

--- shop/src/pages/_app.tsx
+++ shop/src/pages/_app.tsx
@@ -1,10 +1,19 @@
+import { Container, Header } from "@/styles/app"
 import { globalStyles } from "@/styles/global"
 import { AppProps } from "next/app"
+import logoImg from '../assets/logo.svg'
 
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
