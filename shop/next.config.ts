92d95eae7844191a29a6a954745c1c48d3abc57b
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2025-02-14 21:23:46 -0300

    Update next.config.ts

--- shop/next.config.ts
+++ shop/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  },
 };
 
 export default nextConfig;
