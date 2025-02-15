66a59469ed2b0445f0d69b61c74af455da87eb54
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2025-02-15 10:23:25 -0300

    Update index.tsx

--- shop/src/pages/index.tsx
+++ shop/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { GetServerSideProps } from "next"
+import { GetServerSideProps, GetStaticProps } from "next"
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -44,7 +44,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -60,10 +60,11 @@ export const getServerSideProps: GetServerSideProps = async () => {
       price: (price.unit_amount ?? 7990) / 100,
     }
   })
-
+  const REVALIDATION_PAGE_TIME = 60 * 60 * 2
   return {
     props: {
       products
-    }
+    },
+    revalidate: REVALIDATION_PAGE_TIME,
   }
 }
